3cdd52a3870205525b88d4b72b437fbdd1aa9257
Sachin Pant <74765996+pantsachin@users.noreply.github.com>
2024-04-15 13:12:49 +0530

    commit after changes

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://hedmyvht:nsVHOjznQ3DNERnk8yrzX2hwKAJW2d8A@floppy.db.elephantsql.com/hedmyvht");
     await client.connect();
     return client;
 }
